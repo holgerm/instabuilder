23312c04cac95b4191689a33cfa5ab5e2c54d16c
holgerm <mail@holgermuegge.de>
2024-03-10 20:32:24 +0100

    refactored status hud in separate file status_hud.lua

--- mods/builda/init.lua
+++ mods/builda/init.lua
@@ -5,6 +5,7 @@ local builda = {
 _G.builda = builda
 
 dofile(minetest.get_modpath("insta").."/countdown.lua")
+dofile(minetest.get_modpath("insta").."/status_hud.lua")
 
 --builda implements the gameplay logic of Builda City.
 --In this gamemode, players have energy and are required to build
@@ -12,96 +13,6 @@ dofile(minetest.get_modpath("insta").."/countdown.lua")
 --energy supply infrastructure.
 
 local S = minetest.get_translator("builda")
-
-local hud_id_money
-local hud_id_money_icon
-local hud_id_co2
-local hud_id_co2_icon
-local hud_id_population
-local hud_id_population_icon
-
-local function reset_state()
-    for _, player in ipairs(minetest.get_connected_players()) do
-        player:get_meta():set_int("costs", 0)
-        player:get_meta():set_int("population", 0)
-        player:get_meta():set_int("co2", 0)
-        player:hud_change(hud_id_money, "text", 0 .. " / " .. _G.insta.goal_money)
-        player:hud_change(hud_id_co2, "text", 0 .. " / " .. _G.insta.goal_co2)
-        player:hud_change(hud_id_population, "text", 0 .. " / " .. _G.insta.goal_population)
-        -- Change the text color to red
-        player:hud_change(hud_id_population, "number", 0xFF0000)
-        player:hud_change(hud_id_population_icon, "text", "population_red.png")
-    end
-end
-
-_G.builda.Reset_state = reset_state
-
---returns true if the player can afford.
-local AddPlayerCosts = function(player, coins)
-    player:get_meta():set_int("costs", player:get_meta():get_int("costs") + coins);
-    if player:get_meta():get_int("costs") < 0 then
-        player:get_meta():set_int("costs", 0)
-        return false
-    end
-    if hud_id_money then
-        player:hud_change(hud_id_money, "text", player:get_meta():get_int("costs") .. " / " .. _G.insta.goal_money)
-        if player:get_meta():get_int("costs") > _G.insta.goal_money then
-            -- Change the text color to red
-            player:hud_change(hud_id_money, "number", 0xFF0000)
-            player:hud_change(hud_id_money_icon, "text", "cost_red.png")
-
-        else
-            -- Change the text color to white
-            player:hud_change(hud_id_money, "number", 0xFFFFFF)
-            player:hud_change(hud_id_money_icon, "text", "cost.png")
-        end
-    end
-
-    return true
-end
-
-local AddPlayerPopulation = function(player, coins)
-    player:get_meta():set_int("population", player:get_meta():get_int("population") + coins);
-    if player:get_meta():get_int("population") < 0 then
-        player:get_meta():set_int("population", 0)
-        -- Change the text color to red
-        player:hud_change(hud_id_population, "number", 0xFF0000)
-        return false
-    end
-    if hud_id_population then
-        player:hud_change(hud_id_population, "text", player:get_meta():get_int("population") .. 
-            " / " .. _G.insta.goal_population)
-        if player:get_meta():get_int("population") < _G.insta.goal_population then
-            -- Change the text color to red
-            player:hud_change(hud_id_population, "number", 0xFF0000)
-            player:hud_change(hud_id_population_icon, "text", "population_red.png")
-        else
-            player:hud_change(hud_id_population, "number", 0xFFFFFF)
-            player:hud_change(hud_id_population_icon, "text", "population.png")
-        end
-    end
-    return true
-end
-
-local AddPlayerCO2 = function(player, coins)
-    player:get_meta():set_int("co2", player:get_meta():get_int("co2") + coins);
-    if hud_id_co2 then
-        player:hud_change(hud_id_co2, "text", player:get_meta():get_int("co2") ..
-            " / " .. _G.insta.goal_co2)
-        if player:get_meta():get_int("co2") > _G.insta.goal_co2 then
-            -- Change the text color to red
-            player:hud_change(hud_id_co2, "number", 0xFF0000)
-            player:hud_change(hud_id_co2_icon, "text", "co2_red.png")
-        else
-            -- Change the text color to white
-            player:hud_change(hud_id_co2, "number", 0xFFFFFF)
-            player:hud_change(hud_id_co2_icon, "text", "co2.png")
-        end
-    end
-    return true
-end
-
-
 
 minetest.hud_replace_builtin("health", nil)
 
@@ -138,67 +49,8 @@ minetest.register_on_joinplayer(function(player)
     player:hud_set_flags({healthbar=false, breathbar=false, wielditem=false})
     player:hud_set_hotbar_image("builda_empty.png")
 
-    --Brain Icon.
-    hud_id_money_icon = player:hud_add({
-        hud_elem_type = "statbar",
-        position = {x=1, y=0},
-        text = "cost.png",
-        number = 2,
-        size = {x=64, y=64},
-        offset = {x=-64-10, y=5},
-    })
-    --Brain Count
-    hud_id_money = player:hud_add({
-        name = "coins",
-        hud_elem_type = "text",
-        position = {x=1, y=0},
-        text = player:get_meta():get_int("costs") .. " / " .. _G.insta.goal_money,
-        number = 0xffffff,
-        size = {x=3, y=3},
-        offset = {x=-90, y=10},
-        alignment = {x=-1, y=1},
-    })
-    --Energy Count
-    hud_id_co2 = player:hud_add({
-        name = "co2",
-        hud_elem_type = "text",
-        position = {x=1, y=0},
-        text = math.floor(player:get_meta():get_float("co2")+0.5) .. " / " .. _G.insta.goal_co2,
-        number = 0xffffff,
-        size = {x=3, y=3+5},
-        offset = {x=-90, y=64+5},
-        alignment = {x=-1, y=1},
-    })
-    --Energy Icon
-    hud_id_co2_icon = player:hud_add({
-        hud_elem_type = "statbar",
-        position = {x=1, y=0},
-        text = "co2.png",
-        number = 2,
-        size = {x=48, y=48+5},
-        offset = {x=-64, y=64+7},
-    })
-    -- Population Count
-    hud_id_population = player:hud_add({
-        name = "population",
-        hud_elem_type = "text",
-        position = {x=1, y=0},
-        text = math.floor(player:get_meta():get_float("population")+0.5) .. " / " .. _G.insta.goal_population,
-        number = 0xffffff,
-        size = {x=3, y=3+5},
-        offset = {x=-90, y=64+5+64},
-        alignment = {x=-1, y=1},
-    })
-    -- Population Icon
-    hud_id_population_icon = player:hud_add({
-        hud_elem_type = "statbar",
-        position = {x=1, y=0},
-        text = "population_red.png",
-        number = 2,
-        size = {x=48, y=48+5},
-        offset = {x=-64, y=64+7+64},
-    })
-
+    _G.builda.set_status_hud(player)
+    _G.builda.Reset_state()
 
     --Setup camera, the player is inside an energy distrubution craft
     --and is able to fly through single-node spaces.
@@ -291,8 +143,8 @@ minetest.register_item("builda:road", {
         _G.worksaver.print_area()
         if pointed_thing.type == "node" then
             if logistics.place("city:street_off", pointed_thing.above, user) then
-                AddPlayerCosts(user, street_cost)
-                AddPlayerCO2(user, street_co2)
+                _G.builda.AddPlayerCosts(user, street_cost)
+                _G.builda.AddPlayerCO2(user, street_co2)
             end
         end
     end
@@ -310,8 +162,8 @@ minetest.register_item("builda:green", {
 })
 
 local function AddPoints4Green(user, from_level, to_level) 
-    AddPlayerCosts(user, green_cost[to_level] - (green_cost[from_level] or 0))
-    AddPlayerCO2(user, green_co2[to_level] - (green_co2[from_level] or 0))
+    _G.builda.AddPlayerCosts(user, (green_cost[to_level] or 0) - (green_cost[from_level] or 0))
+    _G.builda.AddPlayerCO2(user, (green_co2[to_level] or 0) - (green_co2[from_level] or 0))
 end
 
 _G.builda.AddPoints4Green = AddPoints4Green
@@ -335,9 +187,9 @@ minetest.register_item("builda:residential_concrete", {
 })
 
 local function AddPoints4ResidentialConcrete(user, from_level, to_level)
-    AddPlayerCosts(user, (residential_concrete_cost[to_level] or 0) - (residential_concrete_cost[from_level] or 0))
-    AddPlayerCO2(user, (residential_concrete_co2[to_level] or 0) - (residential_concrete_co2[from_level] or 0))
-    AddPlayerPopulation(user, (residential_concrete_population[to_level] or 0) -
+    _G.builda.AddPlayerCosts(user, (residential_concrete_cost[to_level] or 0) - (residential_concrete_cost[from_level] or 0))
+    _G.builda.AddPlayerCO2(user, (residential_concrete_co2[to_level] or 0) - (residential_concrete_co2[from_level] or 0))
+    _G.builda.AddPlayerPopulation(user, (residential_concrete_population[to_level] or 0) -
         (residential_concrete_population[from_level] or 0))
 end
 
@@ -361,9 +213,9 @@ minetest.register_item("builda:residential_brick", {
 })
 
 local function AddPoints4ResidentialBrick(user, from_level, to_level) 
-    AddPlayerCosts(user, (residential_brick_cost[to_level] or 0) - (residential_brick_cost[from_level] or 0))
-    AddPlayerCO2(user, (residential_brick_co2[to_level] or 0) - (residential_brick_co2[from_level] or 0))
-    AddPlayerPopulation(user, (residential_brick_population[to_level] or 0) -
+    _G.builda.AddPlayerCosts(user, (residential_brick_cost[to_level] or 0) - (residential_brick_cost[from_level] or 0))
+    _G.builda.AddPlayerCO2(user, (residential_brick_co2[to_level] or 0) - (residential_brick_co2[from_level] or 0))
+    _G.builda.AddPlayerPopulation(user, (residential_brick_population[to_level] or 0) -
         (residential_brick_population[from_level] or 0))
 end
 
@@ -387,9 +239,9 @@ minetest.register_item("builda:residential_wood", {
 })
 
 local function AddPoints4ResidentialWood(user, from_level, to_level) 
-    AddPlayerCosts(user, (residential_wood_cost[to_level] or 0) - (residential_wood_cost[from_level] or 0))
-    AddPlayerCO2(user, (residential_wood_co2[to_level] or 0) - (residential_wood_co2[from_level] or 0))
-    AddPlayerPopulation(user, (residential_wood_population[to_level] or 0) -
+    _G.builda.AddPlayerCosts(user, (residential_wood_cost[to_level] or 0) - (residential_wood_cost[from_level] or 0))
+    _G.builda.AddPlayerCO2(user, (residential_wood_co2[to_level] or 0) - (residential_wood_co2[from_level] or 0))
+    _G.builda.AddPlayerPopulation(user, (residential_wood_population[to_level] or 0) -
         (residential_wood_population[from_level] or 0))
 end
 
@@ -420,8 +272,8 @@ minetest.register_item("builda:destroyer", {
             local item = minetest.registered_items[thing.name]
             if item then
                 if thing.name == "city:street_off" then
-                    AddPlayerCosts(user, -street_cost)
-                    AddPlayerCO2(user, -street_co2)
+                    _G.builda.AddPlayerCosts(user, -street_cost)
+                    _G.builda.AddPlayerCO2(user, -street_co2)
                 elseif item.kind == "green" then
                     AddPoints4Green(user, 1, 0)
                 elseif item.kind == "residential_concrete" then
